26835b0b9ab2cd68257a61bd203698a650a7ebd9
prabii <codeprabhas121@gmail.com>
2026-03-19 11:50:23 +0530

    Add package.json, server.js and .gitignore for deployment

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = require("./src/app");
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
